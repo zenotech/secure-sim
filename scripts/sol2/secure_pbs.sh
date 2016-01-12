8be88dd116a76beddbcee9787d98f6d804b1985d
mike-jt79 <mike.turner@zenotech.com>
2016-01-12 11:42:35 +0000

    update to pbs scripts

--- scripts/sol2/secure_pbs.sh
+++ scripts/sol2/secure_pbs.sh
@@ -1,19 +1,17 @@
-export TMP_FS="~/scratch/"
-export INPUT_FILE="~/secure-sim/options/sol2/motorBike.tar.gz.enc"
-export OUTPUT_FILE="./motorbike.tar.gz"
+TMP_FS="/work/d94/d94/shared/secure_sim/scratch/"
+INPUT_FILE="/work/d94/d94/shared/secure-sim/options/sol2/motorBike.tar.gz.enc"
+OUTPUT_FILE="./motorbike.tar.gz"
 
 echo "Running secure submit"
-export NODE_LIST=`cat $PBS_NODEFILE | uniq`
+NODE_LIST=`cat $PBS_NODEFILE | uniq`
 echo "Allocated nodes $NODE_LIST"
-export NODE_COUNT=`cat $PBS_NODEFILE | uniq | wc -l`
+NODE_COUNT=`cat $PBS_NODEFILE | uniq | wc -l`
 
 IPS=""
 for host in $NODE_LIST;
 do
    IPS="$IPS `getent hosts $host | awk '{ print $1 }'`"
 done
-
-echo $IPS
 
 echo "Please enter public key:"
 read pubkey
@@ -22,24 +20,26 @@ echo "Please enter private key:"
 read privkey
 
 echo "Starting Agent"
-echo $NODE_LIST
 aprun -n 1 secure_sim start_agent $pubkey $privkey $INPUT_FILE -c $NODE_COUNT $IPS &
 export AGENT_HOST=`cat $PBS_NODEFILE | uniq | sed -n '1p;'`
 echo $AGENT_HOST
 
-sleep 120
+sleep 10
 echo "Decrypting file to local scratch"
-#srun -m cyclic -n $NODE_COUNT hostname
-#srun -m cyclic -n $NODE_COUNT mkdir -p $TMP_FS
-#srun -D $TMP_FS -m cyclic -n $NODE_COUNT secure_sim decrypt $INPUT_FILE --agent_url=http://$AGENT_HOST:5000 --output $OUTPUT_FILE
+export MPICH_RANK_REORDER_METHOD=0
+aprun -n $NODE_COUNT hostname
+aprun -n $NODE_COUNT mkdir -p $TMP_FS
+aprun -n $NODE_COUNT secure_sim decrypt $INPUT_FILE --agent_url=http://$AGENT_HOST:5000 --output $OUTPUT_FILE
 wait
+export MPICH_RANK_REORDER_METHOD=1
 
 echo "Running Hydra"
-#srun echo "Running..."
-#srun -D $TMP_FS ls
+aprun echo "Running..."
 
 echo "Cleaning up scratch"
-#srun -m cyclic -n $NODE_COUNT rm -rf $TMP_FS
+export MPICH_RANK_REORDER_METHOD=0
+aprun -n $NODE_COUNT rm -rf $TMP_FS
+export MPICH_RANK_REORDER_METHOD=1
 
 echo "Release allocation"
 exit
